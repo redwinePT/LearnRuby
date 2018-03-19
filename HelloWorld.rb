c87c37f8748bcdfb1e573c7dec820443b9f1067a
redwinePT <37539043+redwinePT@users.noreply.github.com>
2018-03-19 13:57:41 +0000

    Create HelloWorld.rb
    Test the GitHub circuit again.

---
+++ HelloWorld.rb
@@ -0,0 +1,19 @@
+#
+require 'rubygems'
+require 'rake'
+require 'YAML'
+include FileUtils
+require 'rexml/document'
+require 'pathname'
+#  Exercise 1: A Good First Program
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts 'Yay! Printing.'
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+
+# Extra Credit
+puts "Make your script print another line."
+#
